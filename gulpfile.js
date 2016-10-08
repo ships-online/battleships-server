1862692fff7261f9af1ed20c980539ba09cf46c9
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-10-08 16:26:42 +0200

    Added core dev tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,16 @@
 'use strict';
 
-const fs = require( 'fs' );
-const gulp = require( 'gulp' );
-const gulpFilter = require( 'gulp-filter' );
-const gulpEslint = require( 'gulp-eslint' );
-
-/**
- * Paths definitions.
- */
-const jsFiles = [ '**/*.js' ].concat( getGitIgnore() );
-
-/**
- * Tasks definitions.
- */
-const tasks = {
-	/**
-	 * Analyze quality and code style of JS files.
-	 *
-	 * @returns {Stream}
-	 */
-	lint() {
-		return gulp.src( jsFiles )
-			.pipe( gulpEslint() )
-			.pipe( gulpEslint.format() )
-			.pipe( gulpEslint.failAfterError() );
-	},
-
-	/**
-	 * Lints staged files - pre commit hook.
-	 *
-	 * @returns {Stream}
-	 */
-	lintStaged() {
-		const guppy = require( 'git-guppy' )( gulp );
-
-		return guppy.stream( 'pre-commit', { base: './' } )
-			.pipe( gulpFilter( jsFiles ) )
-			.pipe( gulpEslint() )
-			.pipe( gulpEslint.format() )
-			.pipe( gulpEslint.failAfterError() );
-	}
+const config = {
+	ROOT_PATH: '.'
 };
 
-/**
- * Gets the list of ignores from `.gitignore`.
- *
- * @returns {Array<String>} The list of ignores.
- */
-function getGitIgnore() {
-	let gitIgnoredFiles = fs.readFileSync( '.gitignore', 'utf8' );
+const gulp = require( 'gulp' );
+const lintTasks = require( 'battle-ships-core/dev/tasks/lint.js' )( config );
+const compileTasks = require( 'battle-ships-core/dev/tasks/compile.js' )( config );
 
-	return gitIgnoredFiles
-		// Remove comment lines.
-		.replace( /^#.*$/gm, '' )
-		// Transform into array.
-		.split( /\n+/ )
-		// Remove empty entries.
-		.filter( ( path ) => !!path )
-		// Add `!` for ignore glob.
-		.map( i => '!' + i );
-}
+// Compile core to common js format.
+gulp.task( 'compile', () => compileTasks.compile( './core', 'cjs' ) );
 
 // JS code sniffer.
-gulp.task( 'lint', tasks.lint );
-gulp.task( 'pre-commit', tasks.lintStaged );
+gulp.task( 'lint', () => lintTasks.lint( '**/*.js' ) );
+gulp.task( 'pre-commit', () => lintTasks.lintStaged( '**/*.js' ) );
