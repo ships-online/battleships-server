25dfc21e1ebf0747684947b5c91bd2afb659e078
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-11 17:56:29 +0100

    Updated meta.

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ const config = {
 const path = require( 'path' );
 const gulp = require( 'gulp' );
 const del = require( 'del' );
-const lintTasks = require( 'battle-ships-engine/dev/tasks/lint.js' )( config );
-const engineCompileTasks = require( 'battle-ships-engine/dev/tasks/compile.js' )( config );
+const lintTasks = require( 'battleships-engine/dev/tasks/lint.js' )( config );
+const engineCompileTasks = require( 'battleships-engine/dev/tasks/compile.js' )( config );
 
 // Compile engine to common js format.
 gulp.task( 'clean:compile', () => del( './engine' ) );
-gulp.task( 'compile:engine', [ 'clean:compile' ], () => engineCompileTasks.compile( '../battle-ships-engine/src', './engine', 'cjs' ) );
+gulp.task( 'compile:engine', [ 'clean:compile' ], () => engineCompileTasks.compile( '../battleships-engine/src', './engine', 'cjs' ) );
 
 // JS code sniffer.
 const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
