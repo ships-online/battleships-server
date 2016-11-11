1a5ccaed76e8b324720ad7b65a29a5b59c0b3eac
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-11 23:32:19 +0100

    Aligned dev tools paths.

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ const config = {
 const path = require( 'path' );
 const gulp = require( 'gulp' );
 const del = require( 'del' );
-const lintTasks = require( 'battleships-engine/dev/tasks/lint.js' )( config );
-const engineCompileTasks = require( 'battleships-engine/dev/tasks/compile.js' )( config );
+const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
+const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
 // Compile engine to common js format.
-gulp.task( 'clean:compile', () => del( './engine' ) );
-gulp.task( 'compile:engine', [ 'clean:compile' ], () => engineCompileTasks.compile( '../battleships-engine/src', './engine', 'cjs' ) );
+gulp.task( 'clean:compile:engine', () => del( './engine' ) );
+gulp.task( 'compile:engine', [ 'clean:compile:engine' ], () => compileTasks.compile( '../battleships-engine/src', './engine', 'cjs' ) );
 
 // JS code sniffer.
 const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
