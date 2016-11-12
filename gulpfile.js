99cedd3a6fc31594803660d7f90a1a5c17f9ee50
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-12 10:34:25 +0100

    Moved compiled files to `lib` directory.

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,13 @@ const del = require( 'del' );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
-// Compile engine to common js format.
-gulp.task( 'clean:compile:engine', () => del( './engine' ) );
-gulp.task( 'compile:engine', [ 'clean:compile:engine' ], () => compileTasks.compile( '../battleships-engine/src', './engine', 'cjs' ) );
+// Compile engine and utils to commonjs format.
+gulp.task( 'clean:compile:engine', () => del( './lib/engine' ) );
+gulp.task( 'clean:compile:utils', () => del( './lib/utils' ) );
+
+gulp.task( 'compile:engine', [ 'clean:compile:engine' ], () => compileTasks.compile( '../battleships-engine/src', './lib/engine', 'cjs' ) );
+gulp.task( 'compile:utils', [ 'clean:compile:utils' ], () => compileTasks.compile( '../battleships-utils/src', './lib/utils', 'cjs' ) );
+gulp.task( 'compile', [ 'compile:engine', 'compile:utils' ], ( done ) => done() );
 
 // JS code sniffer.
 const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
