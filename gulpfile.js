d6af90fc0031aef5c62914fb6a69e3f3a8863344
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-22 02:25:43 +0100

    Refactored dev tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -14,20 +14,31 @@ const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( co
 
 gulp.task( 'relink', linkTask.relink );
 
-// Compile engine and utils to commonjs format.
-gulp.task( 'clean:compile:engine', () => utils.del( './lib/engine' ) );
-gulp.task( 'clean:compile:utils', () => utils.del( './lib/utils' ) );
+// build engine and utils to commonjs format.
+gulp.task( 'clean:build:engine', () => utils.del( './lib/battleships-engine' ) );
+gulp.task( 'clean:build:utils', () => utils.del( './lib/battleships-utils' ) );
 
-gulp.task( 'compile:engine', [ 'clean:compile:engine' ], () => {
-	compileTasks.compile( 'node_modules/battleships-engine/src', './lib/engine', { format: 'cjs' } )
+gulp.task( 'build:engine', [ 'clean:build:engine' ], () => {
+	compileTasks.build(
+		path.join( '.', 'node_modules', 'battleships-engine' ),
+		path.join( '.', 'lib', 'battleships-engine' ),
+		{
+			relativeTo: path.join( '..', '..', path.sep ),
+			format: 'cjs'
+		}
+	);
 } );
-gulp.task( 'compile:utils', [ 'clean:compile:utils' ], () => {
-	compileTasks.compile( 'node_modules/battleships-utils/src', './lib/utils', { format: 'cjs' } )
+
+gulp.task( 'build:utils', [ 'clean:build:utils' ], () => {
+	compileTasks.build(
+		path.join( '.', 'node_modules', 'battleships-utils' ),
+		path.join( '.', 'lib', 'battleships-utils' ),
+		{
+			format: 'cjs'
+		}
+	);
 } );
-gulp.task( 'compile', [ 'compile:engine', 'compile:utils' ], ( done ) => done() );
+gulp.task( 'battleships:build', [ 'build:engine', 'build:utils' ], ( done ) => done() );
 
-// JS code sniffer.
-const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
-
-gulp.task( 'lint', () => lintTasks.lint( jsFiles ) );
-gulp.task( 'pre-commit', () => lintTasks.lintStaged( jsFiles ) );
+gulp.task( 'lint', lintTasks.lint );
+gulp.task( 'pre-commit', lintTasks.lintStaged );
