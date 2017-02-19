2bcdb5694bc5b626b4a47b78f5885d170a18d708
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-02-19 18:06:35 +0100

    Added test task.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ const utils = require( 'battleships-dev-tools/lib/utils.js' );
 const linkTask = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
+const testTasks = require( 'battleships-dev-tools/lib/tasks/test.js' )( config );
+
+const options = utils.parseArgs( process.argv.slice( 3 ) );
 
 gulp.task( 'relink', linkTask.relink );
 
@@ -39,6 +42,8 @@ gulp.task( 'build:utils', [ 'clean:build:utils' ], () => {
 	);
 } );
 gulp.task( 'battleships:build', [ 'build:engine', 'build:utils' ], ( done ) => done() );
+
+gulp.task( 'test', () => testTasks.testNode( options ) );
 
 gulp.task( 'lint', lintTasks.lint );
 gulp.task( 'pre-commit', lintTasks.lintStaged );
