5ce8fb27dd308e9e2ba073a1c67922e8445ee194
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-11-01 18:43:03 +0100

    Aligned code with dev-tools changes.

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,13 @@ const path = require( 'path' );
 const gulp = require( 'gulp' );
 
 const utils = require( 'battleships-dev-tools/lib/utils.js' );
-const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
-const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
+const compile = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
-// Build engine and utils to commonjs format.
 gulp.task( 'clean:engine', () => utils.del( path.join( '.', 'lib', 'battleships-engine' ) ) );
 gulp.task( 'clean:utils', () => utils.del( path.join( '.', 'lib', '@ckeditor' ) ) );
 
 gulp.task( 'compile:engine', [ 'clean:engine' ], () => {
-	return compileTasks.compile(
+	return compile(
 		path.join( '.', 'node_modules', 'battleships-engine', 'src' ),
 		path.join( '.', 'lib', 'battleships-engine', 'src' ),
 		{
@@ -27,7 +25,7 @@ gulp.task( 'compile:engine', [ 'clean:engine' ], () => {
 } );
 
 gulp.task( 'compile:utils', [ 'clean:utils' ], () => {
-	return compileTasks.compile(
+	return compile(
 		path.join( '.', 'node_modules', '@ckeditor', 'ckeditor5-utils', 'src' ),
 		path.join( '.', 'lib', '@ckeditor', 'ckeditor5-utils', 'src' ),
 		{
@@ -35,7 +33,5 @@ gulp.task( 'compile:utils', [ 'clean:utils' ], () => {
 		}
 	);
 } );
-gulp.task( 'compile', [ 'compile:engine', 'compile:utils' ], done => done() );
 
-gulp.task( 'lint', lintTasks.lint );
-gulp.task( 'pre-commit', lintTasks.lintStaged );
+gulp.task( 'compile', [ 'compile:engine', 'compile:utils' ], done => done() );
