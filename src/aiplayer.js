5600897b14d310070e6526cb082642fd1cbbb41f
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-07-03 00:25:14 +0200

    Added docs to the AiPlayer class.

--- src/aiplayer.js
+++ src/aiplayer.js
@@ -14,13 +14,26 @@ const {
 	getPositionsAroundTheShip
 } = require( '../lib/battleships-engine/src/utils/positions.js' );
 
+/**
+ * Not too smart AI :)
+ *
+ * @extends Player
+ */
 class AiPlayer extends Player {
+	/**
+	 * @param {Battlefield} battlefield
+	 * @param {Socket} socket
+	 * @param {Game} game
+	 */
 	constructor( battlefield, socket, game ) {
 		super( battlefield, socket );
 
 		this._game = game;
 	}
 
+	/**
+	 * Starts listening and reacts on game life cycle changes.
+	 */
 	start() {
 		const game = this._game;
 		const ships = this._getShipsConfiguration();
@@ -50,6 +63,12 @@ class AiPlayer extends Player {
 		} );
 	}
 
+	/**
+	 * Returns random configuration of ships on the battlefield.
+	 *
+	 * @private
+	 * @returns {Array<Ship>}
+	 */
 	_getShipsConfiguration() {
 		const battlefield = new PlayerBattlefield( this.battlefield.size, this.battlefield.shipsSchema );
 
@@ -62,18 +81,28 @@ class AiPlayer extends Player {
 		return battlefield.shipsCollection.toJSON();
 	}
 
+	/**
+	 * @private
+	 * @returns {Array<Number, Number>} Position [ x, y ]
+	 */
 	_getShootPosition() {
-		const positions = this._getPositions();
+		const positions = this._getAvailablePositions();
 
 		return positions[ random( 0, positions.length - 1 ) ];
 	}
 
-	_getPositions() {
+	/**
+	 * @private
+	 * @returns {Array<Array>} Array of positions [ x, y ].
+	 */
+	_getAvailablePositions() {
 		const battlefield = this._game.player.battlefield;
 		const size = battlefield.size;
 		const positions = [];
 		let aroundTheShips = [];
 
+		// First try to get position of damaged ships.
+		// If there is no any, then collect positions surrounding sunk ships.
 		for ( const ship of battlefield.shipsCollection ) {
 			if ( !ship.isSunk && ship.damages.some( damage => damage ) ) {
 				return this._getPossibleShipPositions( ship );
@@ -82,8 +111,10 @@ class AiPlayer extends Player {
 			}
 		}
 
+		// Remove duplicated positions.
 		aroundTheShips = uniqWith( aroundTheShips, isEqual );
 
+		// Collect all available positions on the battlefield.
 		for ( let x = 0; x < battlefield.size; x++ ) {
 			for ( let y = 0; y < battlefield.size; y++ ) {
 				const field = battlefield.getField( [ x, y ] );
@@ -94,9 +125,15 @@ class AiPlayer extends Player {
 			}
 		}
 
-		return differenceWith( uniqWith( positions, isEqual ), aroundTheShips, isEqual );
+		// Returns positions excluding positions around sunk ships.
+		return differenceWith( positions, aroundTheShips, isEqual );
 	}
 
+	/**
+	 * @private
+	 * @param {Ship} ship
+	 * @returns {Array<Array>} Array of positions [ x, y ].
+	 */
 	_getPossibleShipPositions( ship ) {
 		const battlefield = this._game.player.battlefield;
 		const size = battlefield.size;
